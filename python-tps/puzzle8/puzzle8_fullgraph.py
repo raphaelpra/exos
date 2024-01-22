a7db5c222829e8325fbeddf3b3c223a6bd504eb2
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2024-01-22 13:13:04 +0100

    puzzle8 can save its graph in about 1s and load it in about 500 ms

--- python-tps/puzzle8/puzzle8_fullgraph.py
+++ python-tps/puzzle8/puzzle8_fullgraph.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import pickle
 
 import numpy as np
 
@@ -18,6 +19,8 @@ NEIGHBOURS = {
 # an array containing 10**8, ... 100, 10, 1
 DIGITS = np.logspace(8, 0, num=9, base=10, dtype=np.uint64)
 
+GRAPH_FILENAME = 'puzzle8.pickle'
+
 class Board:
     def __init__(self, array=None):
         if array is None:
@@ -25,15 +28,29 @@ class Board:
         self.array = np.array(array, dtype=np.uint8)
         # caching the hash value allows to speed up
         # the computation of the graph from 50 to 33 seconds
-        # also, using to_int rather than hash(str(self.array))
-        # allosw to speed it up from 33 to 9 seconds
-        self.h = hash(self.to_int())
+        # also, using to_uint rather than hash(str(self.array))
+        # allows to speed it up from 33 to 9 seconds
+        # even with this more efficient hashing code,
+        # caching the hash allows to speed up from 11.5 to 9 seconds
+        # so we keep it
+        self.h = hash(self.to_uint())
 
-    def to_int(self):
+    def to_uint(self):
         """
         useful to speed up hashing
+        and for serializing
         """
         return np.sum(self.array * DIGITS)
+    # to_int actually returns a uint64, which is not json serializable
+    def to_int(self):
+        return int(self.to_uint())
+    @staticmethod
+    def from_int(i):
+        """
+        useful for (de)serializing
+        """
+        array = i % (DIGITS*10) // DIGITS
+        return Board(array)
 
     def __str__(self):
         return str(self.array)
@@ -46,7 +63,6 @@ class Board:
 
     def iter_moves(self):
         zero_index = np.where(self.array == 0)[0][0]
-        # print(zero_index)
         neighbours_indices = NEIGHBOURS[zero_index]
         for neighbour_index in neighbours_indices:
             board = self.array.copy()
@@ -76,3 +92,20 @@ class Solver:
                 if move not in self.graph:
                     self.graph[move].append(next)
                     queue.append(move)
+
+    def save_graph(self):
+        """
+        save the graph to a file, using pickle because we have instances
+        and JSON does not support instances, so we would have to convert
+        back and forth and that would be slow, ruiing the whole point
+        of saving the graph
+        """
+        with open(GRAPH_FILENAME, 'wb') as f:
+            pickle.dump(self.graph, f)
+
+    @staticmethod
+    def load_graph():
+        solver = Solver()
+        with open(GRAPH_FILENAME, 'rb') as f:
+            solver.graph = pickle.load(f)
+        return solver
